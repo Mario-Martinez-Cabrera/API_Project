0d5ba23c1ead51404ceff811feaec4f2f4d5782d
Mario Martinez <mario3alberto3@gmail.com>
2023-03-07 10:07:02 -0600

    updated views

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -2,5 +2,17 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from django.http import HttpResponse, JsonResponse
+from .models import MenuItem, Category, Cart, Order, OrderItem
+from .serializers import MenuItemSerializer
+from rest_framework import generics
 
 # Create your views here.
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
