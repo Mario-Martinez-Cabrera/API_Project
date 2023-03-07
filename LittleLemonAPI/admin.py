a591397ae4fd91179d867df979a4656938c109f3
Mario Martinez <mario3alberto3@gmail.com>
2023-03-06 23:44:18 -0600

    registered each model in seperate line

--- LittleLemonAPI/admin.py
+++ LittleLemonAPI/admin.py
@@ -3,5 +3,8 @@ from .models import Category, MenuItem, Cart, Order, OrderItem
 
 # Register your models here.
 
-admin.site.register(Category, MenuItem, Cart, Order, OrderItem)
-
+admin.site.register(Category)
+admin.site.register(MenuItem)
+admin.site.register(Cart)
+admin.site.register(Order)
+admin.site.register(OrderItem)
