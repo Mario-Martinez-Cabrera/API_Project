fc4c7e26c5adfedc4129e64927949375a2ec6096
Mario Martinez <mario3alberto3@gmail.com>
2023-03-06 23:42:13 -0600

    registered models in admin,py

--- LittleLemonAPI/admin.py
+++ LittleLemonAPI/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, MenuItem, Cart, Order, OrderItem
 
 # Register your models here.
+
+admin.site.register(Category, MenuItem, Cart, Order, OrderItem)
+
