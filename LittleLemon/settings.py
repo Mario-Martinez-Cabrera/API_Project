2688e14af21a8b29f4a98d66fc78c839045c397e
Mario Martinez <mario3alberto3@gmail.com>
2023-03-07 10:42:25 -0600

    updated DEFAULT_RENDERER_CLASSES

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -137,6 +137,5 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
     'rest_framework.renderers.JSONRenderer',
     'rest_framework.renderers.BrowsableAPIRenderer',
-    'rest_framework\_xml.renderers.XMLRenderer',
     ]
 }
