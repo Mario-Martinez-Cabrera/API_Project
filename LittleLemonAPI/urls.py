4232edc0e45acaa5b381e97168383afbae2fae46
Mario Martinez <mario3alberto3@gmail.com>
2023-03-08 10:43:04 -0600

    added paths

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -2,7 +2,20 @@ from django.urls import path
 from LittleLemonAPI import views
 
 urlpatterns = [
-    path('menuitem/', views.menuitems_list),
-    path('menuitem/<int:id>', views.menuitem_detail),
-    path('cart/', views.cart_list),
+    path('categories', views.CategoriesView.as_view()),
+    path('menu-items', views.MenuItemsView.as_view()),
+    path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('cart/menu-items', views.CartView.as_view()),
+    path('orders', views.OrderView.as_view()),
+    path('orders/<int:pk>', views.SingleOrderView.as_view()),
+    path('groups/manager/users', views.GroupViewSet.as_view(
+        {'get': 'list', 'post': 'create', 'delete': 'destroy'})),
+
+    path('groups/delivery-crew/users', views.DeliveryCrewViewSet.as_view(
+        {'get': 'list', 'post': 'create', 'delete': 'destroy'})),
 ]
+
+### for views not needed. To remember code. ### 
+# path('menuitem/', views.menuitems_list),
+    # path('menuitem/<int:id>', views.menuitem_detail),
+    # path('cart/', views.cart_list),
