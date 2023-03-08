357904dcbea4803598d567415ad83d10d3a6e5dd
Mario Martinez <mario3alberto3@gmail.com>
2023-03-08 10:41:06 -0600

    created DeliveryCrewViewSet with functions

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -145,6 +145,36 @@ class GroupViewSet(viewsets.ViewSet):
         managers = Group.objects.get(name="Manager")
         managers.user_set.remove(user)
         return Response({"message": "user removed from the manager group"}, 200)
+    
+
+class DeliveryCrewViewSet(viewsets.ViewSet):
+    permission_classes = [IsAuthenticated]
+
+    def list(self, request):
+        users = User.objects.all().filter(groups__name='Delivery Crew')
+        items = UserSerializer(users, many=True)
+        return Response(items.data)
+
+    def create(self, request):
+        #only for super admin and managers
+        if self.request.user.is_superuser == False:
+            if self.request.user.groups.filter(name='Manager').exists() == False:
+                return Response({"message":"forbidden"}, status.HTTP_403_FORBIDDEN)
+        
+        user = get_object_or_404(User, username=request.data['username'])
+        dc = Group.objects.get(name="Delivery Crew")
+        dc.user_set.add(user)
+        return Response({"message": "user added to the delivery crew group"}, 200)
+
+    def destroy(self, request):
+        #only for super admin and managers
+        if self.request.user.is_superuser == False:
+            if self.request.user.groups.filter(name='Manager').exists() == False:
+                return Response({"message":"forbidden"}, status.HTTP_403_FORBIDDEN)
+        user = get_object_or_404(User, username=request.data['username'])
+        dc = Group.objects.get(name="Delivery Crew")
+        dc.user_set.remove(user)
+        return Response({"message": "user removed from the delivery crew group"}, 200)
 
 ### Not needed but nice to have and to remember the code ###
 # @api_view(['GET', 'POST'])
