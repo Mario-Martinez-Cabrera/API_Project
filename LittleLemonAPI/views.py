c7e411c14708d0f26a40a5f6e26f49dbf58bed35
Mario Martinez <mario3alberto3@gmail.com>
2023-03-07 20:52:15 -0600

    deleted comments

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -42,13 +42,3 @@ def menuitem_detail(request, id):
         menuitem.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-
-
-
-#class MenuItemsView(generics.ListCreateAPIView):
-#    queryset = MenuItem.objects.all()
-#    serializer_class = MenuItemSerializer
-
-#class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
-#    queryset = MenuItem.objects.all()
-#    serializer_class = MenuItemSerializer
