7b1da71dcbeca2dcdc037693e48fafe52d22029c
Mario Martinez <mario3alberto3@gmail.com>
2023-03-07 18:44:18 -0600

    added to fields

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -4,5 +4,5 @@ from rest_framework import serializers
 class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuItem
-        fields = ['title', 'price', 'feature', 'category']
+        fields = ['id', 'title', 'price', 'feature', 'category']
         extra_kwargs = {'price': {'min_value': 2}}
