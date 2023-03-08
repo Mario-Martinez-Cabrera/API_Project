b8a7c0dbc60fed31eadc85c9df5f0111e3c04b36
Mario Martinez <mario3alberto3@gmail.com>
2023-03-08 09:27:04 -0600

    added filter_backends and pagination_class

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -137,7 +137,15 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
     'rest_framework.renderers.JSONRenderer',
     'rest_framework.renderers.BrowsableAPIRenderer',
-    ]
+    ],
+
+    'DEFAULT_FILTER_BACKENDS': [
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
+    ],
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2
 }
 
 DJOSER = {
