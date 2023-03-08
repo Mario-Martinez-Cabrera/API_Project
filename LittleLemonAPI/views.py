49e43acffea83ab544fe007c44f644d8039827a7
Mario Martinez <mario3alberto3@gmail.com>
2023-03-07 22:10:16 -0600

    added isauthenticated and permission_classes packa

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from django.http import HttpResponse, JsonResponse
 from .models import MenuItem, Cart
 from .serializers import MenuItemSerializer, CartSerializer
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
