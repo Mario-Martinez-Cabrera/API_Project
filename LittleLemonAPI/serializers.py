0c8b8e566ea31037a73e3ad80e194a9dc02b4bfd
Mario Martinez <mario3alberto3@gmail.com>
2023-03-08 09:29:53 -0600

    added CategorySerializer class

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -1,7 +1,13 @@
-from .models import MenuItem, Cart
+from .models import MenuItem, Cart, Category
 from rest_framework import serializers
 from decimal import Decimal
 from django.contrib.auth.models import User
+
+
+class CategorySerializer (serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'title', 'slug']
 
 class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
