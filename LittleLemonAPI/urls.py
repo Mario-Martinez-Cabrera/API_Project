1604ac51108e08806e37fe3a53acbaf7e32787a7
Mario Martinez <mario3alberto3@gmail.com>
2023-03-07 18:54:54 -0600

    added menu-items path

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from . import views
+from LittleLemonAPI import views
 
 urlpatterns = [
-    path('menu-items', views.MenuItemsView.as_view()),
-    path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('menu-items', views.menuitems_list),
 ]
